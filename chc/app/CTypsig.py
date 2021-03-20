6710ca6269db53ea2a1171b1ea59237f6d3c0b58
A. Cody Schuffelen <schuffelen@google.com>
2021-03-20 13:38:49 -0700

    Add some annotations to CTypsig

--- chc/app/CTypsig.py
+++ chc/app/CTypsig.py
@@ -25,17 +25,22 @@
 # SOFTWARE.
 # ------------------------------------------------------------------------------
 
+from typing import List, Tuple, TYPE_CHECKING
+
 import chc.app.CDictionaryRecord as CD
+
+if TYPE_CHECKING:
+    import chc.app.CDictionary
 
 class CTypsigTSBase(CD.CDictionaryRecord):
 
-    def __init__(self,cd,index,tags,args):
+    def __init__(self, cd: 'chc.app.CDictionary.CDictionary', index: int, tags: List[str], args: List[int]) -> None:
         CD.CDictionaryRecord.__init__(self,cd,index,tags,args)
  
 
 class CTypsigArray(CTypsigTSBase):
 
-    def __init_(self,cd,index,tags,args):
+    def __init__(self, cd: 'chc.app.CDictionary.CDictionary', index: int, tags: List[str], args: List[int]) -> None:
         CTypsigTSBase.__init__(self,cd,index,tags,args)
 
     def get_len_opt(self):
@@ -45,30 +50,30 @@ class CTypsigArray(CTypsigTSBase):
 
     def get_attributes(self): return self.cd.get_attributes(self.args[1])
 
-    def __str__(self): return ('tsarray(' + str(self.get_typsig())
+    def __str__(self) -> str: return ('tsarray(' + str(self.get_typsig())
                                    + ',' + str(self.get_len_opt))
 
 class CTypsigPtr(CTypsigTSBase):
 
-    def __init__(self,cd,index,tags,args):
+    def __init__(self, cd: 'chc.app.CDictionary.CDictionary', index: int, tags: List[str], args: List[int]) -> None:
         CTypsigTSBase.__init__(self,cd,index,tags,args)
 
     def get_typsig(self): return self.cd.get_typsig(self.args[0])
 
-    def __str__(self): return ('tsptr(' + str(self.get_typsig()) + ')')
+    def __str__(self) -> str: return ('tsptr(' + str(self.get_typsig()) + ')')
 
 class CTypsigComp(CTypsigTSBase):
 
-    def __init__(self,cd,index,tags,args):
+    def __init__(self, cd: 'chc.app.CDictionary.CDictionary', index: int, tags: List[str], args: List[int]) -> None:
         CTypsigTSBase.__init__(self,cd,index,tags,args)
 
     def get_name(self): return self.tags[1]
 
-    def __str__(self): return ('tscomp(' + str(self.get_name()) + ')')
+    def __str__(self) -> str: return ('tscomp(' + str(self.get_name()) + ')')
 
 class CTypsigFun(CTypsigTSBase):
 
-    def __init__(self,cd,index,tags,args):
+    def __init__(self, cd: 'chc.app.CDictionary.CDictionary', index: int, tags: List[str], args: List[int]) -> None:
         CTypsigTSBase.__init__(self,cd,index,tags,args)
 
     def get_typsig(self): return self.cd.get_typsig(self.args[0])
@@ -77,27 +82,27 @@ class CTypsigFun(CTypsigTSBase):
         ix = self.args[1]
         return (self.cd.get_typsig_list(ix) if ix >= 0 else None)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return ('(' + str(self.get_typsig_list_opt) + '):'
                     + str(self.get_typsig()) + ')')
 
 class CTypsigEnum(CTypsigTSBase):
 
-    def __init__(self,cd,index,tags,args):
+    def __init__(self, cd: 'chc.app.CDictionary.CDictionary', index: int, tags: List[str], args: List[int]) -> None:
         CTypsigTSBase.__init__(self,cd,index,tags,args)
 
     def get_name(self): return self.tags[1]
 
-    def __str__(self): return 'tsenum(' + self.get_name() + ')'
+    def __str__(self) -> str: return 'tsenum(' + self.get_name() + ')'
 
 class CTypsigBase(CTypsigTSBase):
 
-    def __init__(self,cd,index,tags,args):
+    def __init__(self, cd: 'chc.app.CDictionary.CDictionary', index: int, tags: List[str], args: List[int]) -> None:
         CTypsigTSBase.__init__(self,cd,index,tags,args)
 
     def get_type(self): return self.cd.get_typ(self.args[0])
 
-    def __str__(self): return 'tsbase(' + str(self.get_type()) + ')'
+    def __str__(self) -> str: return 'tsbase(' + str(self.get_type()) + ')'
         
 
 class CTypsigList(object):
@@ -113,7 +118,7 @@ class CTypsigList(object):
 
     def get_typsig_list(self): return [ self.cd.get_typsig(ix) for ix in args ]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return ','.join([str(x) for x in self.get_typsig_list()])
 
 
