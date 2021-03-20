5c39b987e97c11d65f54e32687f9d83c383c6b0f
A. Cody Schuffelen <schuffelen@google.com>
2021-03-20 15:31:12 -0700

    Add more type annotations to CDictionary

--- chc/app/CDictionaryRecord.py
+++ chc/app/CDictionaryRecord.py
@@ -28,10 +28,12 @@
 from typing import List, Tuple, TYPE_CHECKING
 import xml.etree.ElementTree as ET
 
+import chc.util.IndexedTable as IT
+
 if TYPE_CHECKING:
     import chc.app.CDictionary
 
-class CDictionaryRecord(object):
+class CDictionaryRecord(IT.IndexedTableValue):
     """Base class for all objects kept in the CDictionary."""
 
     def __init__(self, cd: 'chc.app.CDictionary.CDictionary', index: int, tags: List[str], args: List[int]) -> None:
