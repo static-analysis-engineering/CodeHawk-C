48f7bc2ab043967071a562dc0426d4ed27249820
Henny Sipma <hennysipma@mac.com>
2024-04-22 23:07:29 -0700

    KENDRA: updates and conversions

--- chc/cmdline/kendra/TestResults.py
+++ chc/cmdline/kendra/TestResults.py
@@ -5,8 +5,8 @@
 # The MIT License (MIT)
 #
 # Copyright (c) 2017-2020 Kestrel Technology LLC
-# Copyright (c) 2020-2022 Henny Sipma
-# Copyright (c) 2023      Aarno Labs LLC
+# Copyright (c) 2020-2022 Henny B. Sipma
+# Copyright (c) 2023-2024 Aarno Labs LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -393,7 +393,11 @@ class TestResults(object):
                             if count != "ok":
                                 lines.append("     count: " + count)
                         for (ctxt, hashstr) in missing:
-                            lines.append("      " + str(ctxt) + ": " + str(hashstr))
+                            lines.append(
+                                "      "
+                                + str(ctxt)
+                                + ": "
+                                + str(hashstr))
 
         if self.includes_pevs:
             lines.append("\nCheck primary proof results:\n" + ("-" * 80))
@@ -411,7 +415,7 @@ class TestResults(object):
                         else:
                             lines.append("    " + fname)
                             for (ppo, status, hasmultiple) in pevs:
-                                ctxt = ppo.get_context_string() if hasmultiple else ""
+                                ctxt = ppo.context_string if hasmultiple else ""
                                 lines.append(
                                     "    "
                                     + str(ppo.line).rjust(4)
--- chc/cmdline/kendra/kendrautil.py
+++ chc/cmdline/kendra/kendrautil.py
@@ -5,7 +5,7 @@
 # ------------------------------------------------------------------------------
 # The MIT License (MIT)
 #
-# Copyright (c) 2023  Aarno Labs, LLC
+# Copyright (c) 2023-2024  Aarno Labs, LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -28,10 +28,11 @@
 """Command-line interface to the CodeHawk C Analyzer."""
 
 import argparse
+import json
 import os
 import sys
 
-from typing import List, NoReturn
+from typing import Any, Dict, List, NoReturn, Optional, Set, Tuple
 
 from chc.app.CApplication import CApplication
 from chc.cmdline.kendra.TestManager import TestManager
@@ -44,12 +45,28 @@ import chc.reporting.DictionaryTables as DT
 import chc.reporting.ProofObligations as RP
 
 import chc.util.fileutil as UF
+from chc.util.loggingutil import chklogger, LogLevel
 
 
 def print_error(m: str) -> None:
     sys.stderr.write(("*" * 80) + "\n")
     sys.stderr.write(m + "\n")
     sys.stderr.write(("*" * 80) + "\n")
+
+
+def set_logging(
+        level: str,
+        path: str,
+        logfilename: Optional[str],
+        msg: str = "",
+        mode: str = "a") -> None:
+
+    if level in LogLevel.all() or logfilename is not None:
+        if logfilename is not None:
+            logfilename = os.path.join(path, logfilename)
+
+        chklogger.set_chkc_logger(
+            msg, level=level, logfilename=logfilename, mode=mode)
 
 
 def kendra_list(args: argparse.Namespace) -> NoReturn:
@@ -99,14 +116,31 @@ def kendra_show_set(args: argparse.Namespace) -> NoReturn:
     exit(0)
 
 
-def run_test(testname: str, verbose: bool, line_summary: bool = False) -> None:
+def kendra_clean_set(args: argparse.Namespace) -> NoReturn:
+    """Removes the analysis artifacts of a kendra test set."""
+
+    # arguments
+    testname = args.testset
     try:
-        cpath = UF.get_kendra_testpath(testname)
+        projectpath = UF.get_kendra_testpath(testname)
     except UF.CHError as e:
         print_error(str(e.wrap()))
         exit(1)
 
-    reftestfilename = os.path.join(cpath, testname + ".json")
+    testmanager = TestManager(projectpath, projectpath, testname)
+    testmanager.clean()
+
+    exit(0)
+
+
+def run_test(testname: str, verbose: bool, line_summary: bool = False) -> None:
+    try:
+        projectpath = UF.get_kendra_testpath(testname)
+    except UF.CHError as e:
+        print_error(str(e.wrap()))
+        exit(1)
+
+    reftestfilename = os.path.join(projectpath, testname + ".json")
     if not os.path.isfile(reftestfilename):
         print_error(
             "Test directory does not contain a test specification.\n"
@@ -114,7 +148,8 @@ def run_test(testname: str, verbose: bool, line_summary: bool = False) -> None:
             + reftestfilename)
         exit(1)
 
-    testmanager = TestManager(cpath, cpath, testname, verbose=verbose)
+    testmanager = TestManager(
+        projectpath, projectpath, testname, verbose=verbose)
     testmanager.clean()
 
     try:
@@ -153,13 +188,14 @@ def run_test(testname: str, verbose: bool, line_summary: bool = False) -> None:
 
 
 def kendra_test_set(args: argparse.Namespace) -> NoReturn:
-    """Parses and analyzes a testset and compares the result with the reference results."""
+    """Parses and analyzes a testset and compares the result with a reference"""
 
     # arguments
-    ctestset: str = args.testset
-    logfile: str = args.logfile
-    loglevel: str = args.loglevel
+    testname: str = args.testset
     verbose: bool = args.verbose
+    loglevel: str = args.loglevel
+    logfilename: Optional[str] = args.logfilename
+    logfilemode: str = args.logfilemode
 
     try:
         UF.check_parser()
@@ -168,12 +204,26 @@ def kendra_test_set(args: argparse.Namespace) -> NoReturn:
         print_error(str(e.wrap()))
         exit(1)
 
-    run_test(ctestset, verbose)
+    try:
+        projectpath = UF.get_kendra_testpath(testname)
+    except UF.CHError as e:
+        print_error(str(e.wrap()))
+        exit(1)
+
+
+    set_logging(
+        loglevel,
+        projectpath,
+        logfilename=logfilename,
+        mode=logfilemode,
+        msg="cfile parse invoked")
+
+    run_test(testname, verbose)
     exit(0)
 
 
 skips = [139, 151, 163, 363, 391]
-    
+
 
 def kendra_test_sets(args: argparse.Namespace) -> NoReturn:
 
@@ -195,19 +245,25 @@ def kendra_test_sets(args: argparse.Namespace) -> NoReturn:
 def kendra_report_file(args: argparse.Namespace) -> NoReturn:
 
     # arguments
-    cfilename: str = args.cfilename
+    cfilename_c: str = args.cfilename
     showinvs: bool = args.show_invariants
 
     try:
-        cpath = UF.get_kendra_cpath(cfilename)
+        cpath = UF.get_kendra_cpath(cfilename_c)
     except UF.CHCError as e:
         print(str(e.wrap()))
         exit(1)
 
-    sempath = os.path.join(cpath, "semantics")
+    projectpath = cpath
+    targetpath = projectpath
+    cfilename = cfilename_c[:-2]
+    projectname = os.path.basename(projectpath)
     contractpath = os.path.join(cpath, "chcontracts")
-    cfapp = CApplication(sempath, cfilename, contractpath=contractpath)
-    cfile = cfapp.get_cfile()
+
+    capp = CApplication(
+        projectpath, projectname, targetpath, contractpath, singlefile=True)
+    capp.initialize_single_file(cfilename)
+    cfile = capp.get_cfile()
 
     print(RP.file_code_tostring(cfile, showinvs=showinvs))
     print(RP.file_proofobligation_stats_tostring(cfile))
@@ -218,26 +274,27 @@ def kendra_report_file(args: argparse.Namespace) -> NoReturn:
 def kendra_show_file_table(args: argparse.Namespace) -> NoReturn:
 
     # arguments
-    cfilename: str = args.cfilename
+    cfilename_c: str = args.cfilename
     tablename: str = args.tablename
 
     try:
-        cpath = UF.get_kendra_cpath(cfilename)
+        cpath = UF.get_kendra_cpath(cfilename_c)
     except UF.CHCError as e:
         print(str(e.wrap()))
         exit(1)
 
-    sempath = os.path.join(cpath, "semantics")
-    cfapp = CApplication(sempath, cfilename)
-    if cfapp.has_single_file():
-        cfile = cfapp.get_cfile()
+    projectpath = cpath
+    targetpath = projectpath
+    cfilename = cfilename_c[:-2]
+    projectname = os.path.basename(projectpath)
+    contractpath = os.path.join(cpath, "chcontracts")
 
-        print(str(DT.get_file_table(cfile, tablename)))
+    capp = CApplication(
+        projectpath, projectname, targetpath, contractpath, singlefile=True)
+    capp.initialize_single_file(cfilename)
+    cfile = capp.get_cfile()
 
-    else:
-        print_error(
-            "File not found. Please make sure the test has been analyzed")
-        exit(1)
+    print(str(DT.get_file_table(cfile, tablename)))
 
     exit(0)
 
@@ -245,26 +302,272 @@ def kendra_show_file_table(args: argparse.Namespace) -> NoReturn:
 def kendra_show_function_table(args: argparse.Namespace) -> NoReturn:
 
     # arguments
-    cfilename: str = args.cfilename
+    cfilename_c: str = args.cfilename
     functionname: str = args.functionname
     tablename: str = args.tablename
 
     try:
-        cpath = UF.get_kendra_cpath(cfilename)
+        cpath = UF.get_kendra_cpath(cfilename_c)
     except UF.CHCError as e:
         print(str(e.wrap()))
         exit(1)
 
-    sempath = os.path.join(cpath, "semantics")
-    cfapp = CApplication(sempath, cfilename)
-    if cfapp.has_single_file():
-        cfile = cfapp.get_cfile()
+    projectpath = cpath
+    targetpath = projectpath
+    cfilename = cfilename_c[:-2]
+    projectname = os.path.basename(projectpath)
+    contractpath = os.path.join(cpath, "cXhcontracts")
 
-        print(str(DT.get_function_table(cfile, functionname, tablename)))
+    capp = CApplication(
+        projectpath, projectname, targetpath, contractpath, singlefile=True)
+    capp.initialize_single_file(cfilename)
+    cfile = capp.get_cfile()
 
+    print(str(DT.get_function_table(cfile, functionname, tablename)))
+
+    exit(0)
+
+
+def get_ppo_count(spec: Dict[str, Any]) -> int:
+    """Returns the number of ppos in the spec file."""
+    total = 0
+    for f in spec["cfiles"]:
+        for ff in spec["cfiles"][f]["functions"]:
+            total += len(spec["cfiles"][f]["functions"][ff]["ppos"])
+    return total
+
+
+def get_spo_count(spec: Dict[str, Any]) -> int:
+    """Returns the number of spos in the spec file."""
+    total = 0
+    for f in spec["cfiles"]:
+        for ff in spec["cfiles"][f]["functions"]:
+            if "spos" in spec["cfiles"][f]["functions"][ff]:
+                total += len(spec["cfiles"][f]["functions"][ff]["spos"])
+    return total
+
+
+def get_ppos(spec: Dict[str, Any], f: str) -> List[Any]:
+    ppos: List[Any] = []
+    for ff in spec["cfiles"][f]["functions"]:
+        ppos.extend(spec["cfiles"][f]["functions"][ff]["ppos"])
+    return ppos
+
+
+def get_spos(spec: Dict[str, Any], f: str) -> List[Any]:
+    spos: List[Any] = []
+    for ff in spec["cfiles"][f]["functions"]:
+        if "spos" in spec["cfiles"][f]["functions"][ff]:
+            spos.extend(spec["cfiles"][f]["functions"][ff]["spos"])
+    return spos
+
+
+def get_violation_predicates(spec: Dict[str, Any]) -> Set[str]:
+    predicates: Set[str] = set([])
+    for f in spec["cfiles"]:
+        for ppo in get_ppos(spec, f):
+            status = ppo["tgtstatus"]
+            if status in ["violation", "violation:delegated"]:
+                predicates.add(ppo["predicate"])
+        for spo in get_spos(spec, f):
+            status = spo["tgtstatus"]
+            if status == "violation":
+                predicates.add(ppo["predicate"])
+    return predicates
+
+
+def get_perc(x: int, y: int, width: int) -> str:
+    fmt = "{:>" + str(width) + ".1f}"
+    if y > 0:
+        perc = float(x) / float(y) * 100.0
+        return fmt.format(perc)
     else:
-        print_error(
-            "File not found. Please make sure the test has been analyzed")
-        exit(1)
+        return " - ".rjust(width)
+
+
+def get_status_ppo_counts(spec: Dict[str, Any], status: str) -> Tuple[int, int]:
+    count = 0
+    tgtcount = 0
+    for f in spec["cfiles"]:
+        for ppo in get_ppos(spec, f):
+            if ppo["tgtstatus"] == status:
+                tgtcount += 1
+                if ppo["status"] == status:
+                    count += 1
+    return (count, tgtcount)
+
+
+def get_open_ppo_count(spec: Dict[str, Any]) -> int:
+    count = 0
+    for f in spec["cfiles"]:
+        for ppo in get_ppos(spec, f):
+            if ppo["status"] == "open":
+                count += 1
+    return count
+
+
+def get_safe_ppo_counts(spec: Dict[str, Any]) -> Tuple[int, int]:
+    return get_status_ppo_counts(spec, "safe")
+
+
+def get_safe_ppo_perc(spec: Dict[str, Any], width: int) -> str:
+    (cnt, tgtcnt) = get_safe_ppo_counts(spec)
+    return get_perc(cnt, tgtcnt, width)
+
+
+def get_violation_ppo_counts(spec: Dict[str, Any]) -> Tuple[int, int]:
+    return get_status_ppo_counts(spec, "violation")
+
+
+def get_violation_ppo_perc(spec: Dict[str, Any], width: int) -> str:
+    (cnt, tgtcnt) = get_violation_ppo_counts(spec)
+    return get_perc(cnt, tgtcnt, width)
+
+
+def get_delegated_ppo_counts(spec: Dict[str, Any]) -> Tuple[int, int]:
+    (safecnt, safetgtcnt) = get_status_ppo_counts(spec, "safe:delegated")
+    (vcnt, vtgtcnt) = get_status_ppo_counts(spec, "violation:delegated")
+    return (safecnt + vcnt, safetgtcnt + vtgtcnt)
+
+
+def get_delegated_ppo_perc(spec: Dict[str, Any], width: int) -> str:
+    (cnt, tgtcnt) = get_delegated_ppo_counts(spec)
+    return get_perc(cnt, tgtcnt, width)
+
+
+def get_status_spo_counts(spec: Dict[str, Any], status: str) -> Tuple[int, int]:
+    count = 0
+    tgtcount = 0
+    for f in spec["cfiles"]:
+        for spo in get_spos(spec, f):
+            if spo["tgtstatus"] == status:
+                tgtcount += 1
+                if spo["status"] == status:
+                    count += 1
+    return (count, tgtcount)
+
+
+def get_open_spo_count(spec: Dict[str, Any]) -> int:
+    count = 0
+    for f in spec["cfiles"]:
+        for spo in get_spos(spec, f):
+            if spo["status"] == "open":
+                count += 1
+    return count
+
+
+def get_safe_spo_counts(spec: Dict[str, Any]) -> Tuple[int, int]:
+    return get_status_spo_counts(spec, "safe")
+
+
+def get_safe_spo_perc(spec: Dict[str, Any], width: int) -> str:
+    (cnt, tgtcnt) = get_safe_spo_counts(spec)
+    return get_perc(cnt, tgtcnt, width)
+
+
+def get_violation_spo_counts(spec: Dict[str, Any]) -> Tuple[int, int]:
+    return get_status_spo_counts(spec, "violation")
+
+
+def get_violation_spo_perc(spec: Dict[str, Any], width: int) -> str:
+    (cnt, tgtcnt) = get_violation_spo_counts(spec)
+    return get_perc(cnt, tgtcnt, width)
+
+
+def get_delegated_spo_counts(spec: Dict[str, Any]) -> Tuple[int, int]:
+    (safecnt, safetgtcnt) = get_status_spo_counts(spec, "safe:delegated")
+    (vcnt, vtgtcnt) = get_status_spo_counts(spec, "violation:delegated")
+    return (safecnt + vcnt, safetgtcnt + vtgtcnt)
+
+
+def get_delegated_spo_perc(spec: Dict[str, Any], width: int) -> str:
+    (cnt, tgtcnt) = get_delegated_spo_counts(spec)
+    return get_perc(cnt, tgtcnt, width)
+
+
+def get_ppo_results(spec: Dict[str, Any]) -> str:
+    opencnt = get_open_ppo_count(spec)
+    s_opencnt = "-" if opencnt == 0 else str(opencnt)
+    return (
+        str(get_ppo_count(spec)).rjust(6)
+        + get_safe_ppo_perc(spec, 12)
+        + get_violation_ppo_perc(spec, 12)
+        + get_delegated_ppo_perc(spec, 12)
+        + s_opencnt.rjust(10)
+    )
+
+
+def get_spo_results(spec: Dict[str, Any]) -> str:
+    spocount = get_spo_count(spec)
+    s_spocount = "-" if spocount == 0 else str(spocount)
+    opencnt = get_open_spo_count(spec)
+    s_opencount = "-" if opencnt == 0 else str(opencnt)
+    return (
+        str(spocount).rjust(6)
+        + get_safe_spo_perc(spec, 12)
+        + get_violation_spo_perc(spec, 12)
+        + get_delegated_spo_perc(spec, 12)
+        + str(opencnt).rjust(10)
+    )
+
+
+def dashboard_header() -> str:
+    lines: List[str] = []
+    header = "testcase".ljust(9)
+    headerppos = (
+        "ppos".rjust(6)
+        + "   "
+        + "%safe".center(12)
+        + "%violation".center(12)
+        + "%delegated".center(12)
+        + "%open".center(12)
+    )
+    headerspos = (
+        "spos".rjust(6)
+        + "  "
+        + "%safe".center(12)
+        + "%violation".center(12)
+        + "%delegated".center(12)
+        + "%open".center(12)
+    )
+    headerline2 = (
+        " ".ljust(18)
+        + "proven".center(12)
+        + "proven".center(12)
+        + "-".ljust(32)
+        + "proven".center(12)
+        + "proven".center(12)
+    )
+    lines.append(header + headerppos + headerspos)
+    lines.append(headerline2)
+    lines.append("-" * 150)
+    return "\n".join(lines)
+
+
+def kendra_dashboard(args: argparse.Namespace) -> NoReturn:
+
+    kendrapath = UF.get_kendra_path()
+    testcases = [(i, "id" + str(i) + "Q") for i in range(115, 394, 4)]
+
+    lines: List[str] = []
+
+    lines.append(dashboard_header())
+
+    for (i, t) in testcases:
+        if (i % 100) < 4:
+            lines.append("-" * 120)
+        specfilename = os.path.join(os.path.join(kendrapath, t), t + ".json")
+        with open(specfilename) as fp:
+            spec = json.load(fp)
+        lines.append(
+            t.ljust(9)
+            + get_ppo_results(spec)
+            + "  | "
+            + get_spo_results(spec)
+            + " ".ljust(6)
+            + ",".join(get_violation_predicates(spec)))
+    lines.append("-" * 150)
+
+    print("\n".join(lines))
 
     exit(0)
