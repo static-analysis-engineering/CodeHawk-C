607a7e68a4fc0386646ad974668286418f3fa643
Henny Sipma <hennysipma@mac.com>
2024-05-20 00:19:28 -0700

    LINKER: partially restored

--- chc/linker/CompCompatibility.py
+++ chc/linker/CompCompatibility.py
@@ -4,7 +4,9 @@
 # ------------------------------------------------------------------------------
 # The MIT License (MIT)
 #
-# Copyright (c) 2017 Kestrel Technology LLC
+# Copyright (c) 2017-2019  Kestrel Technology LLC
+# Copyright (c) 2020-2024  Henny B. Sipma
+# Copyright (c) 2024       Aarno Labs LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,32 +27,51 @@
 # SOFTWARE.
 # ------------------------------------------------------------------------------
 
+from typing import Iterator, List, Set, Tuple, TYPE_CHECKING
 
-class CompCompatibility(object):
+if TYPE_CHECKING:
+    from chc.app.CCompInfo import CCompInfo
+    from chc.app.CFieldInfo import CFieldInfo
+
+
+class CompCompatibility:
     """Determines compatibility between two structs."""
 
-    def __init__(self, comp1, comp2):
-        self.comp1 = comp1  # CCompInfo
-        self.comp2 = comp2  # CCompInfo
+    def __init__(self, comp1: "CCompInfo", comp2: "CCompInfo") -> None:
+        self._comp1 = comp1
+        self._comp2 = comp2
 
-    """Return true if they have the same fields (but not necessarily compatible field types)"""
+    @property
+    def comp1(self) -> "CCompInfo":
+        return self._comp1
 
-    def are_structurally_compatible(self):
-        if self.comp1.getfieldcount() == self.comp2.getfieldcount():
-            return set(self.comp1.getfieldnames()) == set(self.comp2.getfieldnames())
+    @property
+    def comp2(self) -> "CCompInfo":
+        return self._comp2
+
+    def are_structurally_compatible(self) -> bool:
+        """Returns true if comp1 and comp2 have the same fields.
+
+        Note: The field types are not necessarily compatible.
+        """
+        if self.comp1.fieldcount == self.comp2.fieldcount:
+            return set(self.comp1.fieldnames) == set(self.comp2.fieldnames)
         return False
 
-    """Return true if they are structurally compatible and if the types of their fields
-    are structurally compatible, taking into account a list of pairs of struct tags
-    that are explicitly declared incompatible."""
+    def are_shallow_compatible(
+            self, incompatibles: Set[Tuple[str, str]] = set([])) -> bool:
+        """Returns true if comp1 and comp2 are structurally compatible.
 
-    def are_shallow_compatible(self, incompatibles=set([])):
+        Note: This means the the types of comp1 and comp2 are structurally
+        compatible taking into account a list of pairs of struct tags that
+        are explicitly declared incompatible.
+        """
         if self.are_structurally_compatible():
             return self._forall_shallowcompatiblefieldtype(incompatibles)
         return False
 
-    def __str__(self):
-        if self.comp1.getid() == self.comp2.getid():
+    def __str__(self) -> str:
+        if self.comp1.index == self.comp2.index:
             return "identical"
         if self.are_shallow_compatible():
             return "shallow compatible (without incompatibles declared)"
@@ -58,20 +79,23 @@ class CompCompatibility(object):
             return self._find_incompatiblefieldtype()
         return "not structurally compatible"
 
-    def _find_incompatiblefieldtype(self):
-        fieldpairs = zip(sorted(self.comp1.getfields()), sorted(self.comp2.getfields()))
-        for ((_, finfo1), (_, finfo2)) in fieldpairs:
-            t1 = finfo1.gettype()
-            t2 = finfo2.gettype()
-            if not t1.shallowcompatible(t2):
+    def _find_incompatiblefieldtype(self) -> str:
+        fieldpairs: Iterator[Tuple["CFieldInfo", "CFieldInfo"]] = zip(
+            self.comp1.fields, self.comp2.fields)
+        for (finfo1, finfo2) in fieldpairs:
+            t1 = finfo1.ftype
+            t2 = finfo2.ftype
+            if not t1.equal(t2):
                 return "t1: " + str(t1) + "\nt2: " + str(t2)
         return "no incompatible fields found"
 
-    def _forall_shallowcompatiblefieldtype(self, incompatibles=set([])):
-        fieldpairs = zip(sorted(self.comp1.getfields()), sorted(self.comp2.getfields()))
-        for ((_, finfo1), (_, finfo2)) in fieldpairs:
-            t1 = finfo1.gettype().expand()
-            t2 = finfo2.gettype().expand()
-            if not t1.shallowcompatible(t2, incompatibles):
+    def _forall_shallowcompatiblefieldtype(
+            self, incompatibles: Set[Tuple[str, str]] = set([])) -> bool:
+        fieldpairs: Iterator[Tuple["CFieldInfo", "CFieldInfo"]] = zip(
+            self.comp1.fields, self.comp2.fields)
+        for (finfo1, finfo2) in fieldpairs:
+            t1 = finfo1.ftype.expand()
+            t2 = finfo2.ftype.expand()
+            if not t1.equal(t2):
                 return False
         return True
